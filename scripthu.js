0d51e51c4d77113ecfa25a604c66dd2bd4bd2fb2
alexandrekirpa <40843009+alexandrekirpa@users.noreply.github.com>
2019-06-07 14:57:12 -0300

    Update scripthu.js

--- scripthu.js
+++ scripthu.js
@@ -28,8 +28,82 @@ if(na!=null){na=na.toString().replace(/<pre>SÓDIO\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.
 k=cont.match(/<pre>POTÁSSIO\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\: <b>.*?&nbsp;mEq\/L<\/b>/);
 if(k!=null){k=k.toString().replace(/<pre>POTÁSSIO\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\: <b>/gi,'').replace(/&nbsp;mEq\/L<\/b>/gi,'')}else{k='nulo'};
 
-a=[hb,leuco,plq,pcr,ur,cr,na,k];
-b=['Hb','Leuco','Plaquetas','PCR','Ur','Cr','Na','K'];
+ddimero=cont.match(/ D-Dímero\.\.\.\.\.\.\.\.\.\.: <b>.*? ng\/ml<\/b>/);
+if(ddimero!=null){ddimero=ddimero.toString().replace(/ D-Dímero\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/ ng\/ml<\/b>/gi,'')}else{ddimero='nulo'};
+
+ckmb=cont.match(/<pre>CKMB\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;ng\/mL<\/b><\/pre>/);
+if(ckmb!=null){ckmb=ckmb.toString().replace(/<pre>POTÁSSIO\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\: <b>/gi,'').replace(/&nbsp;mEq\/L<\/b>/gi,'')}else{ckmb='nulo'};
+
+troponina=cont.match(/TROPONINA\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;pg\/mL<\/b><br>/);
+if(troponina!=null){troponina=troponina.toString().replace(/TROPONINA\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;pg\/mL/gi,'')}else{troponina='nulo'};
+
+bt=cont.match(/<pre>BILIRRUBINA TOTAL\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;mg\/dL<\/b><\/pre>/);
+if(bt!=null){bt=bt.toString().replace(/<pre>BILIRRUBINA TOTAL\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mg\/dL<\/b><\/pre>/gi,'')}else{bt='nulo'};
+
+bd=cont.match(/<pre>BILIRRUBINA DIRETA\.\.\.\.\.\.\.\.\.\.:<b>.*?&nbsp;mg\/dL<\/b><\/pre>/);
+if(bd!=null){bd=bd.toString().replace(/<pre>BILIRRUBINA DIRETA\.\.\.\.\.\.\.\.\.\.:<b>/gi,'').replace(/&nbsp;mg\/dL<\/b><\/pre>/gi,'')}else{bd='nulo'};
+
+bi=cont.match(/<pre>BILIRRUBINA INDIRETA\.\.\.\.\.\.\.\.: <b>.*?&nbsp;mg\/dL<\/b><\/pre>/);
+if(bi!=null){bt=bt.toString().replace(/<pre>BILIRRUBINA INDIRETA\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mg\/dL<\/b><\/pre>/gi,'')}else{bi='nulo'};
+
+tgo=cont.match(/<pre>ASPARTATO TRANSAMINASE (TGO\/AST)\.\.\.\.: <b>.*?&nbsp;U\/L<\/b><\/pre>/);
+if(tgo!=null){tgo=tgo.toString().replace(/<pre>ASPARTATO TRANSAMINASE (TGO\/AST)\.\.\.\.: <b>/gi,'').replace(/&nbsp;U\/L<\/b><\/pre>/gi,'')}else{tgo='nulo'};
+
+tgp=cont.match(/<pre>ALANINA TRANSAMINASE (TGP\/ALT)\.\.\.\.: <b>.*?&nbsp;U\/L<\/b><\/pre>/);
+if(tgp!=null){tgp=tgp.toString().replace(/<pre>ALANINA TRANSAMINASE (TGP\/ALT)\.\.\.\.: <b>/gi,'').replace(/&nbsp;U\/L<\/b><\/pre>/gi,'')}else{tgp='nulo'};
+
+ggt=cont.match(/<pre>GAMA GT\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;U\/L<\/b><\/pre>/);
+if(ggt!=null){ggt=ggt.toString().replace(/<pre>GAMA GT\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;U\/L<\/b><\/pre>/gi,'')}else{ggt='nulo'};
+
+fa=cont.match(/<pre>FOSFATASE ALCALINA\.\.\.\.\.: <b>.*?&nbsp;U\/L<\/b><\/pre>/);
+if(fa!=null){fa=fa.toString().replace(/<pre>FOSFATASE ALCALINA\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;U\/L<\/b><\/pre>/gi,'')}else{fa='nulo'};
+
+amilase=cont.match(/<pre>ALFA AMILASE\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;U\/L<\/b><\/pre>/);
+if(amilase!=null){amilase=amilase.toString().replace(/<pre>ALFA AMILASE\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;U\/L<\/b><\/pre>/gi,'')}else{amilase='nulo'};
+
+lipase=cont.match(/LÍPASE\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;U\/L<\/b><br>/);
+if(lipase!=null){lipase=lipase.toString().replace(/LÍPASE\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;U\/L<\/b><br>/gi,'')}else{lipase='nulo'};
+
+ph=cont.match(/PH\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;<\/b>/);
+if(ph!=null){ph=ph.toString().replace(/PH\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;<\/b>/gi,'')}else{ph='nulo'};
+
+po2=cont.match(/PO2\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;mmHg<\/b><br>/);
+if(po2!=null){po2=po2.toString().replace(/PO2\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mmHg<\/b><br>/gi,'')}else{po2='nulo'};
+
+hco3=cont.match(/HCO3\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;mmol\/L<\/b><br>/);
+if(hco3!=null){hco3=hco3.toString().replace(/HCO3\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mmol\/L<\/b><br>/gi,'')}else{hco3='nulo'};
+
+pco2=cont.match(/PCO2\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;mmHg<\/b><br>/);
+if(pco2!=null){pco2=pco2.toString().replace(/PCO2\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mmHg<\/b><br>/gi,'')}else{pco2='nulo'};
+
+be=cont.match(/B\.E\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;mmol\/L<\/b><br>/);
+if(be!=null){pco2=pco2.toString().replace(/B\.E\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mmol\/L<\/b><br>/gi,'')}else{pco2='nulo'};
+
+sato2=cont.match(/SAT\. O2\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;%<\/b><br>/);
+if(sato2!=null){sato2=sato2.toString().replace(/SAT\. O2\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;%<\/b><br>/gi,'')}else{sato2='nulo'};
+
+cai=cont.match(/CÁLCIO IONIZADO - SANGUE\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>.8?&nbsp;mmol\/L<\/b><br>/);
+if(cai!=null){cai=cai.toString().replace(/CÁLCIO IONIZADO - SANGUE\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mmol\/L<\/b><br>/gi,'')}else{cai='nulo'};
+
+lactato=cont.match(/<pre>LACTATO\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?&nbsp;mg\/dL<\/b><\/pre>/);
+if(lactato!=null){lactato=lactato.toString().replace(/<pre>LACTATO\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'').replace(/&nbsp;mg\/dL<\/b><\/pre>/gi,'')}else{lactato='nulo'};
+
+var urina;
+urina_leuco=cont.match(/LEUCÓCITOS \.\.\.\.\.\.\.\.: <b>.*?<\/td>/).toString().replace(/<\/td>/gi,'').replace(/LEUCÓCITOS \.\.\.\.\.\.\.\.: <b>/gi,'');
+urina_hemacias=cont.match(/HEMÁCIAS \.\.\.\.\.\.\.\.\.\.: <b>.*?<\/td>/).toString().replace(/<\/td>/gi,'').replace(/HEMÁCIAS \.\.\.\.\.\.\.\.\.\.: <b>/gi,'');
+urina_nitrito=cont.match(/NITRITO\.\.\.\.\.\.\.\.\.\.\.\.: <b>.*?<\/b><\/td>/).toString().replace(/<\/b><\/td>/gi,'').replace(/NITRITO\.\.\.\.\.\.\.\.\.\.\.\.: <b>/gi,'');
+urina_sangue=cont.match(/SANGUE OCULTO \.\.\.\.\.: <b>.*?<\/b><\/td>/).toString().replace(/<\/b><\/td>/gi,'').replace(/SANGUE OCULTO \.\.\.\.\.: <b>.*?<\/b><\/td>/gi,'');
+
+if(urina!=null){
+urina='Leuco (urina): '+urina_leuco+' | Hemacias (urina): '+urina_hemacias+' | Nitrito: '+ urina_nitrito+ ' | Sangue oculto (urina): ' + urina_sangue;
+
+}else{urina='nulo'};
+
+
+
+
+a=[hb,leuco,plq,pcr,ur,cr,na,k,ddimero,ckmb,troponina,bt,bd,bi,tgo,tgp,ggt,fa,amilase,lipase,ph,po2,hco3,pco2,be,sato2,cai,lactato,urina];
+b=['Hb','Leuco','Plaquetas','PCR','Ur','Cr','Na','K','DDIMERO','CKMB','TROPONINA','BT','BD','BI','TGO','TGP','GGT','FA','AMILASE','LIPASE','pH','pO2','BE','SatO2','CAi','Lactato','Urina 1 - '];
 msg='Exames --->';
 
 for(i=0;i<a.length;i++){
